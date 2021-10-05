edca6c9b1e81fdc4f4536d30964146fb95daac10
Carlos Aurélio <carlosync@gmail.com>
2021-10-05 19:15:11 -0400

    Crianção do projeto Webservice Spring Boot

---
+++ src/main/java/com/sw2you/wscourse/WsCourseApplication.java
@@ -0,0 +1,13 @@
+package com.sw2you.wscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sw2you/wscourse/WsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw2you.wscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
